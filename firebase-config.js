757b1e22a2f5ad6e1e367b26280c938ee7ca99ad
Erkraker <118482710+Erkraker@users.noreply.github.com>
2026-05-18 22:44:37 -0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// V7 - Firebase centralizado
+// Se um dia precisar trocar de projeto, mexe só aqui.
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyB2riIg3_tgaGya1BkGJymO4rNFULJCnyw",
+  authDomain: "controle-linhas.firebaseapp.com",
+  projectId: "controle-linhas",
+  storageBucket: "controle-linhas.firebasestorage.app",
+  messagingSenderId: "832921211525",
+  appId: "1:832921211525:web:4a493a33b22bb4f6490fec"
+};
